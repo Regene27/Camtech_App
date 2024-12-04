796af5aaf41dcbbca94ba2b905c5e4ed01f636b2
Line Regene <l.regene27@gmail.com>
2024-12-04 13:52:39 +0700

    Add telegram bot alert

--- camtechAppBackend/appBackend/views.py
+++ camtechAppBackend/appBackend/views.py
@@ -1,73 +1,38 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-from .models import Submission
 from .serializers import SubmissionSerializer
 import logging
 from django.conf import settings
 from django.http import HttpResponse, Http404
-from rest_framework.parsers import MultiPartParser
 import os
 from django.core.mail import send_mail
-from django.conf import settings
 from django.shortcuts import render
-import pyrebase
-import firebase_admin
-from firebase_admin import credentials, firestore
-import json
 from googleapiclient.discovery import build
 from google.oauth2.service_account import Credentials
 import os
-from google.oauth2.service_account import Credentials
-from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-from pydrive2.auth import GoogleAuth
-from pydrive2.drive import GoogleDrive
-from gspread import authorize
-from oauth2client.service_account import ServiceAccountCredentials
-from io import BytesIO
 import tempfile
+import requests
+import environ
+from datetime import datetime
+from pathlib import Path
 
-# Set the environment variable for Google credentials
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = (
-    "/home/regene/Internship/camtechApp/camtechAppBackend/google-sheets-key.json"
-)
+current_date = datetime.now().strftime("%Y-%m-%d")
+current_time = datetime.now().strftime("%H:%M")
 
 
 logger = logging.getLogger(__name__)
 
-firebaseConfig = {
-    "apiKey": "AIzaSyBAxkLjhNU0L1i0LW57fDiIUs-JcUObSKA",
-    "authDomain": "camtech-app-76d33.firebaseapp.com",
-    "projectId": "camtech-app-76d33",
-    "storageBucket": "camtech-app-76d33.appspot.com",
-    "messagingSenderId": "890703025917",
-    "appId": "1:890703025917:web:526eaeed6c8e825c115094",
-    "measurementId": "G-N9HMVNEE9M",
-    "databaseURL": "https://camtech-app-76d33-default-rtdb.asia-southeast1.firebasedatabase.app",
-}
-firebase = pyrebase.initialize_app(firebaseConfig)
-authe = firebase.auth()
-database = firebase.database()
-
-
-cred = credentials.Certificate(
-    "/home/regene/Internship/camtechApp/camtechAppBackend/firebase-key.json"
-)
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
-
-
 SERVICE_ACCOUNT_FILE = (
     "/home/regene/Internship/camtechApp/camtechAppBackend/google-sheets-key.json"
 )
+
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
 ]
 
-# The ID of your Google Spreadsheet (create a blank sheet manually and get its ID from the URL)
 SPREADSHEET_ID = "1T_nMffYCMnt8Lzi9n5XMYVOoTQSJsAIlGPUL0yxH0kM"
 
 ID_FOLDER = "1EnhjTqG-hXYUCGXMhhvX4KKAZtDNcLiS"
@@ -75,12 +40,12 @@ HIGHSCHOOL_CERTIFICATES_FOLDER = "1_SjpJJrOVKlp3-ARGRLugIxtHblh7_1p"
 HIGHSCHOOL_ID_FOLDER = "1O3Q-G1LaroTBloaKLbzKOrbi9YN0Aukj"
 IELTS_FOLDER = "1HWvAKVifbw4xiCVGuUXQAO5_e5tIMu5v"
 
+TELEGRAM_BOT_TOKEN = settings.TELEGRAM_BOT_TOKEN
+TELEGRAM_CHAT_ID = settings.TELEGRAM_CHAT_ID
+
 
 def home(request):
-    submissions = database.child("submissions").get().val()
-    if not submissions:
-        submissions = []
-    return render(request, "home.html", {"submissions": submissions})
+    return render(request, "home.html")
 
 
 def send_confirmation_email(to_email, subject, message):
@@ -103,16 +68,16 @@ def submit_form(request):
             message = "Thank you for applying to Cambodia University of Technology and Science! We have received your admission form, and our admissions team is now reviewing your application.\n\nWhat to Expect Next:\n    •  Our team will assess your application and may reach out if additional documents or information are required.\n    •  We will notify you about the next steps in the application process. You can expect to hear from us soon.\n\nNeed Assistance? If you have any questions, please contact us at info@camtech.edu.kh.\n\nThank you for your interest in Cambodia University of Technology and Science. We wish you success in your application journey and look forward to the possibility of welcoming you to our university community!"
 
             try:
-                # Send confirmation email
                 send_confirmation_email(to_email, subject, message)
 
-                serializer.save()  # Save the serializer data
+                serializer.save()
 
-                # Respond to the front end that the form is submitted
                 response = Response(serializer.data, status=status.HTTP_201_CREATED)
 
-                # Export data after responding to the front end
                 export_data(serializer)
+
+                telegram_message = f"New submission from {serializer.validated_data.get('nameEn')}, {serializer.validated_data.get('gender')}, {serializer.validated_data.get('dob')}. {current_date} : {current_time}"
+                send_telegram_message(telegram_message)
 
                 return response
             except Exception as e:
@@ -138,7 +103,6 @@ def your_view_function(request, filename):
 
 
 def export_data(serializer):
-    # Authenticate with Google Sheets API
     credentials = Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES
     )
@@ -157,7 +121,6 @@ def export_data(serializer):
     highschoolId_link = get_file_link("highschoolIdFile", HIGHSCHOOL_ID_FOLDER)
     ielts_link = get_file_link("IELTSCertificate", IELTS_FOLDER)
 
-    # Get the data to export
     data = []
 
     obj = serializer.validated_data
@@ -167,7 +130,7 @@ def export_data(serializer):
             obj.get("nameKh", "") or "",
             obj.get("nationality", "") or "",
             obj.get("gender", "") or "",
-            str(obj.get("dob", "")) or "",  # Convert date to string
+            str(obj.get("dob", "")) or "",
             obj.get("pob", "") or "",
             obj.get("address", "") or "",
             obj.get("country", "") or "",
@@ -207,7 +170,6 @@ def export_data(serializer):
     )
 
     try:
-        # Write data to Google Sheets
         body = {"values": data}
         service.spreadsheets().values().append(
             spreadsheetId=SPREADSHEET_ID,
@@ -229,7 +191,6 @@ def upload_and_get_link(file_name, file_content, folder_id):
     )
     drive_service = build("drive", "v3", credentials=credentials)
 
-    # Save the file content to a temporary file
     with tempfile.NamedTemporaryFile(delete=False) as temp_file:
         temp_file.write(file_content)
         temp_file_path = temp_file.name
@@ -244,9 +205,14 @@ def upload_and_get_link(file_name, file_content, folder_id):
             .execute()
         )
 
-        # Generate shared link
         shared_link = f"https://drive.google.com/file/d/{file['id']}/view"
         return shared_link
     finally:
-        # Clean up the temporary file
         os.remove(temp_file_path)
+
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {"chat_id": TELEGRAM_CHAT_ID, "text": message, "parse_mode": "HTML"}
+    response = requests.post(url, data=payload)
+    return response
